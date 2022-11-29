316b0bd1ffe4ccdf4e2b567d037c0fa40427f724
klchoi <66675595+klchoi@users.noreply.github.com>
2022-11-30 06:08:24 +0700

    Update countof.fish

--- functions/countof.fish
+++ functions/countof.fish
@@ -1,6 +1,12 @@
 function countof -a dir -d "count direct child directories and video files"
-  count $dir/*/ \
-    $dir/*.mp4 $dir/*.MP4 \
-    $dir/*.mkv $dir/*.MKV \
-    $dir/*.avi $dir/*.AVI
+  set -l count ( count $dir/*.{mp4,MP4,mkv,MKV,avi,AVI} )
+  for d in $dir/*/
+    switch ( path basename $d )
+    case '_*'
+      set count ( math $count + ( countof $dir ) )
+    case '*'
+      count $d/*.{mp4,MP4,mkv,MKV,avi,AVI,jpg,JPG,jpeg,JPEG,png,PNG} >/dev/null && set count ( math $count + 1 )
+    end
+  end
+  echo $count
 end
